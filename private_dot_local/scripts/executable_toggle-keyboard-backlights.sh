75dbb5ed182fd2abc9fb3f37704d897cfe72c517
sgoudham <sgoudham@gmail.com>
2022-12-16 02:08:20 +0000

    feat: script to toggle keyboard backlighting

--- private_dot_local/scripts/executable_toggle-keyboard-backlights.sh
+++ private_dot_local/scripts/executable_toggle-keyboard-backlights.sh
@@ -2,14 +2,14 @@
 
 OFF=0
 ON=2
-PATH=/sys/devices/platform/dell-laptop/leds/dell::kbd_backlight/brightness
-PREVIOUS_SETTING=$(< $PATH)
+KEYBOARD_PATH=/sys/devices/platform/dell-laptop/leds/dell::kbd_backlight/brightness
+PREVIOUS_SETTING=$(< $KEYBOARD_PATH)
 
 if [[ $PREVIOUS_SETTING -eq $OFF ]]
 then
-  echo $ON > $PATH
+  echo $ON > $KEYBOARD_PATH
   echo "Keyboard Backlighting: ON"
 else
-  echo $OFF > $PATH
+  echo $OFF > $KEYBOARD_PATH
   echo "Keyboard Backlighting: OFF"
 fi
