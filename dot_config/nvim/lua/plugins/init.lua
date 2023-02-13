8292443f166eb426a9a4b01072efaf012d2ca8a4
sgoudham <sgoudham@gmail.com>
2023-02-13 03:27:03 +0000

    feat(nvim): add vimtex

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,8 +1,13 @@
 -- misc plugins
 return {
-  { "kana/vim-textobj-entire", dependencies = { "kana/vim-textobj-user" } },
+  {
+    "kana/vim-textobj-entire",
+    dependencies = { "kana/vim-textobj-user" },
+  },
+  -- "bqf" = "better quickfix window"
   "kevinhwang91/nvim-bqf",
   "stevearc/dressing.nvim",
   "mong8se/actually.nvim",
   "andweeb/presence.nvim",
+  "lervag/vimtex",
 }
