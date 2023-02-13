85569311544fbc5dc6f856a4adcc8fbefcd3e4db
sgoudham <sgoudham@gmail.com>
2023-02-13 03:26:24 +0000

    refactor(nvim): disable animation for mini.indentscope

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -5,16 +5,15 @@ return {
       -- add gc/gcc actions, gc textobject
       require("mini.comment").setup({})
 
-      -- autochange dir
-      -- require("mini.misc").setup({})
-      -- MiniMisc.setup_auto_root()
-
       -- highlight word under cursor
       require("mini.cursorword").setup({ delay = 0 })
 
       -- indent lines, `i` textobject, `]i`/`[i` motions
       require("mini.indentscope").setup({
-        draw = { delay = 0 },
+        draw = {
+          delay = 0,
+          animation = require("mini.indentscope").gen_animation.none(),
+        },
         symbol = "│",
       })
     end,
