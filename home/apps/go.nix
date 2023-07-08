029fd6fc0856e0ad27825c65114b59072ed9e3af
sgoudham <sgoudham@gmail.com>
2023-07-08 19:55:25 +0100

    refactor(go): set `GOBIN`

--- home/apps/go.nix
+++ home/apps/go.nix
@@ -2,8 +2,9 @@
   programs.go.enable = true;
   home.sessionVariables = {
     GOPATH = "${config.xdg.dataHome}/go";
+    GOBIN = "${config.home.sessionVariables.GOPATH}/bin";
   };
   home.sessionPath = [
-    "${config.home.sessionVariables.GOPATH}/bin"
+    "${config.home.sessionVariables.GOBIN}"
   ];
 }
