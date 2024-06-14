9199b1cbbd322884149b0e902b6178e0d84e1d82
sgoudham <sgoudham@gmail.com>
2024-06-14 22:54:50 +0100

    refactor: change whiskers input & add catppuccin cachix

--- flake.nix
+++ flake.nix
@@ -14,17 +14,19 @@
 
     git-view.url = "github:sgoudham/git-view/v1.0.0";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    catppuccin-toolbox.url = "github:catppuccin/toolbox";
+    catppuccin-whiskers.url = "github:catppuccin/whiskers";
   };
 
   nixConfig = {
     extra-substituters = [
       "https://nekowinston.cachix.org"
       "https://nix-community.cachix.org"
+      "https://catppuccin.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nekowinston.cachix.org-1:lucpmaO+JwtoZj16HCO1p1fOv68s/RL1gumpVzRHRDs="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
     ];
     extra-trusted-users = ["@wheel"];
     warn-dirty = false;
@@ -38,14 +40,14 @@
     nix-index-database,
     neovim-nightly-overlay,
     git-view,
-    catppuccin-toolbox,
+    catppuccin-whiskers,
     ...
   }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     overlays = final: prev: {
       git-view = git-view.packages.${system}.default;
-      whiskers = catppuccin-toolbox.packages.${pkgs.system}.whiskers;
+      whiskers = catppuccin-whiskers.packages.${pkgs.system}.default;
       nur = import nur {
         nurpkgs = prev;
         pkgs = prev;
