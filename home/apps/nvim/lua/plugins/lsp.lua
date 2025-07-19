06d89d84b34e56760c7baf94dea113850a0bdcdc
sgoudham <sgoudham@gmail.com>
2025-07-19 16:57:57 +0100

    chore(nvim): use tab for selection in blink.cmp

--- home/apps/nvim/lua/plugins/lsp.lua
+++ home/apps/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,26 @@
 return {
   {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "enter",
+        ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
+      },
+      completion = {
+        list = {
+          selection = {
+            preselect = false,
+            auto_insert = true,
+          },
+        },
+        ghost_text = {
+          enabled = false,
+        },
+      },
+    },
+  },
+  {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
     init = function()
